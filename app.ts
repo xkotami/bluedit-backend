c8e8812730b867769066a14b4121b0400309d7c6
Eren Derinbay <eren.derinbay@student.ucll.be>
2025-03-10 14:12:47 +0100

    Added full-stack skeleton

---
+++ app.ts
@@ -0,0 +1,21 @@
+import * as dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import * as bodyParser from 'body-parser';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+const app = express();
+dotenv.config();
+const port = process.env.APP_PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/status', (req, res) => {
+    res.json({ message: 'Back-end is running...' });
+});
+
+app.listen(port || 3000, () => {
+    console.log(`Back-end is running on port ${port}.`);
+});
